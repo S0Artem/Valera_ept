a9991f3b517da8ed3f93947ca8d65c27a97e24c1
S0Artem <so.artem998@gmail.com>
2024-09-13 11:48:15 +0300

    !2! практическая

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,11 +8,7 @@ class Controller
     public function showProducts()
     {
 
-        $products = [
-            ["name" => "Orange", "cost" => 50000000, "amount" => 27],
-            ["name" => "Banana", "cost" => 120000000, "amount" => 17],
-            ["name" => "Bread", "cost" => 70000000, "amount" => 0],
-        ];
+        $products = products::all();
 
         return view('products', ['products' => $products]); // Передаем массив продуктов в представление
     }
